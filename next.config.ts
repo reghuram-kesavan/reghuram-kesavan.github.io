327d3aabdf8b0ba32f77f382b4c86a7b1697fc96
reghuram-kesavan <rgkreghu989@gmail.com>
2026-01-21 02:54:01 +0100

    Fix config type

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: 'export',
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
