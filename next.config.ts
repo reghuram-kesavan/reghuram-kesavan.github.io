5e7ac8d70cb654a0ad84055a49ccc8a377566539
reghuram-kesavan <rgkreghu989@gmail.com>
2026-01-21 02:57:57 +0100

    Optimize for Vercel Deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  output: 'export',
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
